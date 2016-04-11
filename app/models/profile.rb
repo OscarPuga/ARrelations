abae7d2f79acc83d93353e20f016965e340f8213
oscarpuga <oscar.puga.rey@gmail.com>
2016-04-11 10:23:15 +0000

    rq11: validators

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,13 @@
 class Profile < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+    validates :gender, inclusion: { in: %w(male female) }
+    validate :exist_one_name, :badnamed_male
+    
+    def exist_one_name
+        errors.add(:first_name, "at last one of the names must be set") if !(first_name.present?) && !(last_name.present?)
+    end
+    
+    def badnamed_male
+        errors.add(:first_name, "bad name for given gender") if first_name.present? && first_name.start_with?("Sue")
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+    validates :username, presence: true
+
     has_one :profile
     has_many :todo_lists, :dependent => :delete_all
     has_many :todo_items, :through => :todo_lists, :source => :todo_items
+    
+
 end

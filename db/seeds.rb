1e18b7e0d7c992edd4e52f5c30a87ec16a6e3718
oscarpuga <oscar.puga.rey@gmail.com>
2016-04-11 08:41:51 +0000

    seeds database as rq09

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = [
+    ["Carly Fiorina", 1954],
+    ["Donald Trump", 1946],
+    ["Ben Carson", 1951],
+    ["Hillary Clinton", 1947]
+]
+
+tasks5 = []
+
+one_year_date = Date.today + 1.year
+
+5.times { |n| tasks5.push({title: "todo " + n.to_s, description: "must be finished at " + n.to_s, completed: false, due_date: one_year_date }) }
+
+users.each do |data|
+    name = data[0]
+    year = data[1]
+    # byebug
+    user =  User.create({username: name.split(" ")[1], password_digest: "123"})
+    firstname = name.split(" ")[0]
+    profile = Profile.create(
+        {first_name: firstname, last_name: name.split(" ")[1], birth_year: year, gender: firstname[-1]=="y"? "female" : "male"})
+    user.profile = profile
+    
+    user.todo_lists.create({list_name: firstname + "' tasks", list_due_date: one_year_date })
+    
+    tasks5.each { |t|  user.todo_lists.first.todo_items.create(t) }
+    
+end
+
